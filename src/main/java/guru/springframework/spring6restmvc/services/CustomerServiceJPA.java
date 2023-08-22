ac4140a35bf518dc97b547d842dc7eb84b49fddd
Ganbit <ganbit666@o2.pl>
2023-08-22 15:02:28 +0200

    End - 99 (JPA Services)

---
+++ src/main/java/guru/springframework/spring6restmvc/services/CustomerServiceJPA.java
@@ -0,0 +1,50 @@
+package guru.springframework.spring6restmvc.services;
+
+import guru.springframework.spring6restmvc.mappers.CustomerMapper;
+import guru.springframework.spring6restmvc.model.CustomerDTO;
+import guru.springframework.spring6restmvc.repositories.CustomerRepositories;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+@Primary
+@RequiredArgsConstructor
+public class CustomerServiceJPA implements CustomerService {
+    private final CustomerRepositories customerRepositories;
+    private final CustomerMapper customerMapper;
+
+    @Override
+    public List<CustomerDTO> getAllCustomers() {
+        return null;
+    }
+
+    @Override
+    public Optional<CustomerDTO> getCustomerById(UUID uuid) {
+        return Optional.empty();
+    }
+
+    @Override
+    public CustomerDTO saveNewCustomer(CustomerDTO customer) {
+        return null;
+    }
+
+    @Override
+    public void updateCustomerById(UUID id, CustomerDTO customer) {
+
+    }
+
+    @Override
+    public void deleteById(UUID customerId) {
+
+    }
+
+    @Override
+    public void patchById(UUID customerId, CustomerDTO customer) {
+
+    }
+}
